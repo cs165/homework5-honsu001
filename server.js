b13880827bc443186abb734207dd0e3264ea458c
honsu001 <43789499+honsu001@users.noreply.github.com>
2019-06-01 20:32:14 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1qQZjAdrUow4sQHAZY2giGEVq0_6vDv8iFnE_RjyHWnk';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -18,40 +18,161 @@ async function onGet(req, res) {
   const result = await sheet.getRows();
   const rows = result.rows;
   console.log(rows);
+  
+  const data = [];
+  let data_row = {};
+  
+  for(let i=1; i < rows.length; i++ ){
+	  data_row = {};
+	  for(let j=0; j < rows[0].length; j++ ){
+		  data_row[ rows[0][j] ] = rows[i][j];
+	  }
+	  data.push(data_row);
+  }
 
-  // TODO(you): Finish onGet.
-
-  res.json( { status: 'unimplemented'} );
+  res.json(data);
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
   const messageBody = req.body;
-
-  // TODO(you): Implement onPost.
-
-  res.json( { status: 'unimplemented'} );
+  
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  
+  let new_data = [];
+  
+  console.log("input: " + messageBody);
+  for(let column_name in messageBody){
+	  if(column_name.toLowerCase() === "name"){
+		  //name = messageBody[column_name];
+		  new_data.push(messageBody[column_name]);
+	  }
+	  else if(column_name.toLowerCase() === "email"){
+		  //email = messageBody[column_name];
+		  new_data.push(messageBody[column_name]);
+	  }
+  }
+  
+  if(new_data.length === 2){
+	  await sheet.appendRow(new_data);
+	  res.json( {response: 'success'} );
+	  console.log("Post success.");
+  }
+  else{
+	  res.json( {response: 'fail, name or email not found.'} );
+	  console.log("Post failed.");
+  }
 }
 app.post('/api', jsonParser, onPost);
 
 async function onPatch(req, res) {
-  const column  = req.params.column;
+  let column  = req.params.column;
+  column = column.toLowerCase();
   const value  = req.params.value;
   const messageBody = req.body;
-
-  // TODO(you): Implement onPatch.
-
-  res.json( { status: 'unimplemented'} );
+  
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  
+  let column_index = null;
+  let row_index = null;
+  let new_data = [];
+  
+  for(let i=0; i < rows[0].length ; i++){
+	  if(column === rows[0][i].toLowerCase() ){
+		  column_index = i;
+		  break;
+	  }
+  }
+  
+  if(column_index != null){
+	for(let i=1; i < rows.length ; i++){
+		if(value === rows[i][column_index] ){
+			row_index = i;
+			break;
+		}
+	}
+  }
+  else{
+	  res.json( {response: 'fail, column name not exist.'} );
+	  console.log("Patch failed.");
+  }
+  
+  if(row_index != null){
+	  if(column === "name"){
+		  new_data.push(rows[row_index][column_index]);
+		  for(let column_name in messageBody){
+			if(column_name.toLowerCase() === "email"){
+				new_data.push(messageBody[column_name]);
+				break;
+			}
+		  }
+	  }
+	  else if(column === "email"){
+		  for(let column_name in messageBody){
+			if(column_name.toLowerCase() === "name"){
+				new_data.push(messageBody[column_name]);
+				break;
+			}
+		  }
+		  new_data.push(rows[row_index][column_index]);
+	  }
+	  
+	  if(new_data.length === 2){
+		  await sheet.setRow(row_index, new_data);
+		  res.json( {response: 'success'} );
+		  console.log("Patch success.");
+	  }
+	  else{
+		  res.json( {response: 'fail, new data not found.'} );
+		  console.log("Patch failed.");
+	  }
+  }
+  else{
+	  res.json( {response: 'fail, data not exist.'} );
+	  console.log("Patch failed.");
+  }
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
-  const column  = req.params.column;
+  let column  = req.params.column;
   const value  = req.params.value;
-
-  // TODO(you): Implement onDelete.
-
-  res.json( { status: 'unimplemented'} );
+  column = column.toLowerCase();
+  
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  let column_index = null;
+  let row_index = null;
+  
+  for(let i=0; i < rows[0].length ; i++){
+	  if(column === rows[0][i].toLowerCase() ){
+		  column_index = i;
+		  break;
+	  }
+  }
+  if(column_index === null){
+	  res.json( {response: 'fail, column name not exist.'} );
+	  console.log("Delete failed.");
+  }
+  else{
+	  for(let i=1; i < rows.length; i++){
+		  if(value === rows[i][column_index]){
+			  row_index = i;
+			  break;
+		  }
+	  }
+	  if(row_index === null){
+		  res.json( {response: 'fail, data not exist.'} );
+		  console.log("Delete failed.");
+	  }
+	  else{
+		  await sheet.deleteRow(row_index);
+		  res.json( {response: 'success'} );
+		  console.log("Delete success.");
+	  }
+  }
 }
 app.delete('/api/:column/:value',  onDelete);
 
@@ -60,5 +181,5 @@ app.delete('/api/:column/:value',  onDelete);
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log(`Server listening on port ${port}!`);
+  console.log(`CS193X: Server listening on port ${port}!`);
 });
